6e973bf0226b9120d1bf126e4d7f6bffe1a0066a
Kwendoo <antoine.lagna@gmail.com>
2021-11-02 16:41:47 +0100

    Attribute componenet

---
+++ DemoAngular/src/app/demo/demo4/demo4.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo4',
+  templateUrl: './demo4.component.html',
+  styleUrls: ['./demo4.component.scss']
+})
+export class Demo4Component implements OnInit {
+
+  maVar : number = 0;
+  isClickable : boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  changeValue(){
+    this.maVar = 42;
+    this.isClickable = true;
+  }
+
+  change() : boolean{
+    if(this.maVar>5){
+      this.isClickable=true;
+    } else{
+      this.isClickable=false;
+    }
+    return this.isClickable
+  }
+
+}
