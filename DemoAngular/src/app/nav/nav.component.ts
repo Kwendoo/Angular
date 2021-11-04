0a908fa11d2849d54e7a26975d446758f8b7dce1
Kwendoo <antoine.lagna@gmail.com>
2021-11-04 16:40:35 +0100

    Exo directive
    Mise en place d'un navigation avec sous menu

--- DemoAngular/src/app/nav/nav.component.ts
+++ DemoAngular/src/app/nav/nav.component.ts
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
+  listLien : Link[];
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.listLien = [
+      {url : '/home', title : 'Home'},
+      {url : '/about', title : 'About'},
+      {title : 'Demos', children : [
+        {url : '/demo/demo1', title : 'Demo 1 - Binding One-Way'},
+        {url : '/demo/demo2', title : 'Demo 2 - Binding Two-Way'},
+        {url : '/demo/demo3', title : 'Demo 3 - Event Binding'},
+        {url : '/demo/demo4', title : 'Demo 4 - Attribute Binding'},
+        {url : '/demo/demo5', title : 'Demo 5 - Pipes'},
+        {url : '/demo/demo6', title : 'Demo 6 - Component Directives'},
+        {url : '/demo/demo7', title : 'Demo 7 - Structural Directives'},
+      ]},
+      {title : 'Exercices', children : [
+        {url : '/exercice/exo2-chronometer', title : 'Exo 1 - Chronomètre'}
+      ]}
+    ]
+
   }
 
+  toggleVisible(index : number){
+    this.listLien[index].isVisible = !this.listLien[index].isVisible;
+  }
+
+}
+
+
+export class Link{
+  title : string;
+  url? : string;
+  children? : Link[];
+  isVisible? : boolean;
 }
