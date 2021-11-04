eca1b5ae2a1c66c6bbd23b1e06503a3db56eb293
Kwendoo <antoine.lagna@gmail.com>
2021-11-04 14:34:57 +0100

    Component directives

--- DemoAngular/src/app/demo/demo6/demo6.component.ts
+++ DemoAngular/src/app/demo/demo6/demo6.component.ts
@@ -7,9 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Demo6Component implements OnInit {
 
+  currentStyle : any;
+  italic : boolean = false;
+  bold : boolean = false;
+  size : boolean = false;
+
+  class : boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.setCurrentStyle();
+
+  }
+
+  setCurrentStyle(){
+    // this.currentStyle = {'font-style' : 'italic'}
+    // this.currentStyle = {'font-weight' : 'bold'}
+    this.currentStyle = {
+      'font-size' : this.size ? '24px' : '12px',
+      'font-weight' : this.bold ? 'bold' : 'normal',
+      'font-style' : this.italic ? 'italic' : 'normal'
+    }
+  }
+
+  switchBold(){
+    this.bold = !this.bold;
+    this.setCurrentStyle();
+  }
+
+  switchItalic(){
+    this.italic = !this.italic;
+    this.setCurrentStyle();
+  }
+
+  switchSize(){
+    this.size = !this.size;
+    this.setCurrentStyle();
+  }
+
+  switchClass(){
+    this.class = !this.class;
   }
 
 }
